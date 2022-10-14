cbf76a58a27531a9a3178745c92724d338667342
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2022-10-14 10:03:11 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 echo "Dhanraj@007.com" | docker login --username dhanrajranvirkar --password-stdin
-docker build -t dhanrajranvirkar/testapp1:1 .
+docker build -t dhanrajranvirkar/testapp1:${BUILD_NUMBER} .
+docker build -t dhanrajranvirkar/testapp1 .
+docker run -d -p 80:80 dhanrajranvirkar/testapp1
+docker push dhanrajranvirkar/testapp1:${BUILD_NUMBER}
